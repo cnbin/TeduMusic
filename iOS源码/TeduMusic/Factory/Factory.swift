d427e245c4bcd766fe3a8eaf8ff052b778e453b3
jiyingxin <736907613@qq.com>
2015-12-13 12:05:14 +0800

    重新创建项目

--- iOS源码/TeduMusic/Factory/Factory.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Factory.swift
-//  TeduMusic
-//
-//  Created by jiyingxin on 15/12/13.
-//  Copyright © 2015年 yingxin. All rights reserved.
-//
-
-import Foundation
--- iOS源码/TeduMusic/TeduMusic/ViewController/发现/DiscoveryVC.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DiscoveryVC.swift
-//  TeduMusic
-//
-//  Created by jiyingxin on 15/12/13.
-//  Copyright © 2015年 yingxin. All rights reserved.
-//
-
-import Foundation
